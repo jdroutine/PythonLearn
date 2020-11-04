4e95ccdb4b81025cfabdce7aa4271901941a6508
jdroutine <jd.routine@gmail.com>
2020-11-04 18:59:05 +0000

    Exaercise 16 done

---
+++ pythonBasicExercises/16absValue.py
@@ -0,0 +1,9 @@
+number = int(input('Enter The Number To Compare with 17: '))
+difference = number - 17
+
+if number > 17:
+    print(difference)
+    print(abs(difference) * 2)
+elif number <= 17:
+    print(difference)
+   
