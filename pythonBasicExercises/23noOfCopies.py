c28ecb1a5a34e815d6af3b75f4a836b08bebea5c
jdroutine <jd.routine@gmail.com>
2021-01-17 10:57:24 +0000

    ex 23 edited

--- pythonBasicExercises/23noOfCopies.py
+++ pythonBasicExercises/23noOfCopies.py
@@ -1,5 +1,8 @@
 def no_of_copies(text, copies):
-    return text[0:2] * copies
+    if len(text) >= 2:
+        return text[0:2] * copies
+    else:
+        return text * copies
 
 text = input('Enter the character string: ')
 copies = int(input('How many copied you want  to print?: '))
