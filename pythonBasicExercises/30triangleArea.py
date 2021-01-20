e4308de7c68ad9931c84e22a0a79eeb3cffb3f4f
jdroutine <jd.routine@gmail.com>
2021-01-20 22:16:04 +0000

    exercise 30 done

---
+++ pythonBasicExercises/30triangleArea.py
@@ -0,0 +1,7 @@
+def count_area(a, h):
+    return 0.5 * a * h
+
+a = int(input('Enter the value of the triangle base: '))
+h = int(input('Enter the value of the triangle height: '))
+
+print('area = ', count_area(a,h))
